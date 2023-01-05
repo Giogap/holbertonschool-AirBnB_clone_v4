7d966ae9e7df8bbc5db81596f5e50ffd24c0d750
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-01-05 12:09:24 -0500

    update-style

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,11 @@
 $(function () {
-    const amenityId = {};
-  
+  const amenityId = {};
     $('.amenities input[type=checkbox]').change(function () {
-      if (this.checked) {
-        amenityId[$(this).data('name')] = $(this).data('id');
-      } else {
-        delete amenityId[$(this).data('name')];
-      }
-  
-      $('.amenities h4').text(Object.keys(amenityId).join(', '));
-    });
+    if (this.checked) {
+      amenityId[$(this).data('name')] = $(this).data('id');
+    } else {
+      delete amenityId[$(this).data('name')];
+    }
+    $('.amenities h4').text(Object.keys(amenityId).join(', '));
   });
+});
