fcb973ef4b6355c25f51c83a7af502854d80fe2f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-01-05 12:19:53 -0500

    update-style

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,8 +8,8 @@ $(function () {
     }
     $('.amenities h4').text(Object.keys(amenityId).join(', '));
   });
-  $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
-    if (data.status == OK){
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
       $('div#api_status').addClass('available');
     } else {
       $('div#api_status').removeClass('available');
